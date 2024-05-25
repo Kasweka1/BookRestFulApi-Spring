10fb4794b6eeb681a2d9517f5eaeee83dff69e7d
XtremeNation32 <mikeshatas32@gmail.com>
2024-05-25 17:08:11 +0200

    Worked on the user models as well as login and registration

--- src/main/java/com/example/BookApp/user/controller/UserController.java
+++ src/main/java/com/example/BookApp/user/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.BookApp.user.Others.UserRegistrationDto;
 import com.example.BookApp.user.model.User;
 import com.example.BookApp.user.service.LoginRequestDto;
 import com.example.BookApp.user.service.UserService;
--- src/main/java/com/example/BookApp/user/controller/UserRegistrationDto.java
+++
@@ -1,5 +0,0 @@
-package com.example.BookApp.user.controller;
-
-public interface UserRegistrationDto {
-
-}
