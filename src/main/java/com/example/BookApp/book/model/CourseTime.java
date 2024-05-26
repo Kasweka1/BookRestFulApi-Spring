610a5d41efe6aabeb3ca6b97a0c087072b26cf80
Francis Kalunga <kalungafrancis28@gmail.com>
2024-05-26 08:55:23 +0200

    Removed unnecessary imports

--- src/main/java/com/example/BookApp/book/model/CourseTime.java
+++ src/main/java/com/example/BookApp/book/model/CourseTime.java
@@ -3,9 +3,6 @@ package com.example.BookApp.book.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.Column;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 
 @Entity
 @Table(name = "course_times")
