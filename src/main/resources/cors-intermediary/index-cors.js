b5c0bfdfce4e35d054655ffe21fa48ac8f33d222
Francis Kalunga <kalungafrancis28@gmail.com>
2024-06-12 11:48:05 +0200

    file clean up

--- src/main/resources/cors-intermediary/index-cors.js
+++ src/main/resources/cors-intermediary/index-cors.js
@@ -16,116 +16,7 @@ const streams = {
   net: "Computer Networking"
 }
 
-const courseUnits = [
-  {
-    time: '08:00',
-    day: 'Monday',
-    venue: 'SLH',
-    courseCode: 'CSC3600',
-    streams: ['Software Engineering'],
-    type: 'Class'
-  },
-  {
-    time: '09:00',
-    day: 'Monday',
-    venue: 'GLT',
-    courseCode: 'CSC3600',
-    streams: ['Software Engineering'],
-    type: 'Class'
-  },
-  {
-    time: '14:00',
-    day: 'Monday',
-    venue: 'CL2',
-    courseCode: 'CSC3600',
-    streams: ['Software Engineering'],
-    type: 'Lab'
-  },
-  {
-    time: '08:00',
-    day: 'Tuesday',
-    venue: 'GLT',
-    courseCode: 'CSC3011',
-    streams: ['Software Engineering', 'Computer Systems'],
-    type: 'Class'
-  },
-  {
-    time: '11:00',
-    day: 'Tuesday',
-    venue: 'GLT',
-    courseCode: 'CSC3301',
-    streams: [
-      'Software Engineering',
-      'Computer Systems',
-      'Computer Networking'
-    ],
-    type: 'Class'
-  },
-  {
-    time: '08:00',
-    day: 'Wednesday',
-    venue: 'CL2',
-    courseCode: 'CSC3210',
-    streams: ['Computer Systems'],
-    type: 'Class'
-  },
-  {
-    time: '12:00',
-    day: 'Wednesday',
-    venue: 'SLH',
-    courseCode: 'CSC3301',
-    streams: ['Computer Systems'],
-    type: 'Class'
-  },
-  {
-    time: '08:00',
-    day: 'Thursday',
-    venue: 'GLT',
-    courseCode: 'CSC3801',
-    streams: [
-      'Software Engineering',
-      'Computer Systems',
-      'Computer Networking'
-    ],
-    type: 'Class'
-  },
-  {
-    time: '09:00',
-    day: 'Thursday',
-    venue: 'GLT',
-    courseCode: 'CSC3801',
-    streams: [
-      'Software Engineering',
-      'Computer Systems',
-      'Computer Networking'
-    ],
-    type: 'Class'
-  },
-  {
-    time: '08:00',
-    day: 'Friday',
-    venue: 'CL2',
-    courseCode: 'CSC3600',
-    streams: ['Software Engineering'],
-    type: 'Class'
-  },
-  {
-    time: '09:00',
-    day: 'Friday',
-    venue: 'CL2',
-    courseCode: 'CSC3600',
-    streams: ['Software Engineering'],
-    type: 'Class'
-  }
-];
-
-
 app.get('/api/streams', async (req, res) => {
-  /*
-  let streams = await fetch('http://localhost:8080/api/scheduler/streams')
-    .then(response => response.json())
-    .then(results => results)
-  */
   res.json(streams);
 });
 
@@ -137,7 +28,7 @@ app.get('/api/coursetimes/:stream', async (req, res) => {
     .then(response => response.json())
     .then(objectArray => objectArray.filter(object => object["streams"].includes(stream)))
 
-    res.json(filteredCourseUnits);
+  res.json(filteredCourseUnits);
 })
 
 // Start the server
